b2dca10546db1e26c0ef072c15e72de4c9e825f6
Eric Shen <eric.yan.shen@gmail.com>
2026-05-18 21:20:37 -0700

    strassen uses heap; more readable and much slower

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # set to path of jar
-java -jar $(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/build/libs/recaf-1.0.1.jar "$@"
+/home/ericshen/.sdkman/candidates/java/25.0.3-graal/bin/java -jar $(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/build/libs/recaf-1.0.1.jar "$@"
 
